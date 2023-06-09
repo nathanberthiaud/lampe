7cd388e6ac535ae47fb59706e4c515d57d00c899
nathanberthiaud <136059442+nathanberthiaud@users.noreply.github.com>
2023-06-09 08:51:20 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        allumer_la_lampe = !(allumer_la_lampe)
+    } else if (allumer_la_lampe) {
+        pins.analogWritePin(AnalogPin.P0, 1023)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+    if (receivedNumber == 2) {
+        music.playMelody("E B C5 A B G A F ", 300)
+    }
+    if (receivedNumber == 3) {
+        music.playMelody("B A G A G F A C5 ", 300)
+    }
+})
+let allumer_la_lampe = false
+radio.setGroup(300)
